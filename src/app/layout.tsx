184792c58d54cb78375b61dd1fd8dfe856198daa
Salekh Mahmood <salekh.mahmood1@gmail.com>
2025-10-14 10:42:04 +0100

    google ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,11 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	const isDev = process.env.NODE_ENV !== "production";
+  const isProd = !isDev;
 	return (
 		<html lang="en">
 			<head>
-				{isDev ? (
+        {isDev ? (
 					<script
 						dangerouslySetInnerHTML={{
 							__html:
@@ -36,10 +37,40 @@ export default function RootLayout({
 						}}
 					/>
 				) : null}
+        {isProd ? (
+          <>
+            {/* Google Tag Manager */}
+            <script
+              dangerouslySetInnerHTML={{
+                __html:
+                  "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KQRWCCC9');",
+              }}
+            />
+            {/* Google tag (gtag.js) */}
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-WT85V9E262" />
+            <script
+              dangerouslySetInnerHTML={{
+                __html:
+                  "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-WT85V9E262');",
+              }}
+            />
+          </>
+        ) : null}
 				<link rel="apple-touch-icon" href="/twilightnew.png" />
 				<link rel="icon" href="/twilightnew.png" />
 			</head>
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {isProd ? (
+          // Google Tag Manager (noscript)
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-KQRWCCC9"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        ) : null}
 				<ClientScrollTop />
 				{children}
 				<WhatsAppFab />
